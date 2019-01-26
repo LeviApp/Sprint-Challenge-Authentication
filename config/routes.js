c813e4c2a60899c2a3075751b40d83657c88c412
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-01-26 12:53:53 -0700

    Function register added

--- config/routes.js
+++ config/routes.js
@@ -3,12 +3,13 @@ const {server} = require('../api/server.js')
 const express = require('express')
 const knex = require('knex')
 const axios = require('axios');
-const bcrypt = require('bcrypt')
+const bcrypt = require('../node_modules/bcryptjs')
 const dbConfig = require('../knexfile.js')
 
 const db = knex(dbConfig[process.env.DEV])
 const jwt = require('jsonwebtoken')
 const { authenticate } = require('../auth/authenticate');
+const secret = "gold";
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -16,12 +17,42 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
-function register(req, res) {
+function generateToken(user) {
 
+  const payload = {
+    username: user.username
+  }
+
+  const options = {
+    expiresIn: '10min',
+    jwtid:'4040'
+  }
+
+  return jwt.sign(payload,secret,options)
+}
+
+function register(req, res) {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password,16)
+
+    db('users').insert(user)
+      .then(ids => {
+        const id = ids[0];
+        db('users')
+        .where({id})
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+
+          res.status(201).json({message: `user ${user.username} added`, id: user.id, token})
+        })
+        .catch(err => {
+          res.status(500).json({message: `Could not add user! ${err}`})
+        })
+      })
 }
 
 function login(req, res) {
-  // implement user login
 }
 
 function getJokes(req, res) {
